3fa84f8c76d2e1f495dd0379a30b171a5f55e134
Auno-cloud <auno.original1@gmail.com>
2025-06-11 00:18:44 -0300

    Alterando dados do banco de dados

--- mecanic/models.py
+++ mecanic/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Categoria"
         verbose_name_plural = "Categorias"
-    name = models.CharField(max_length=50, verbose_name="Nome")
+    name = models.CharField(max_length=50, verbose_name="Nome", unique=True)
     description = models.TextField(blank=True, null=True, verbose_name="Descrição")
 
 
@@ -37,7 +37,7 @@ class Product(models.Model):
     quantity = models.IntegerField(verbose_name="Quantidade")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
     description = models.TextField(blank=True, null=True, verbose_name="Descrição")
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name="Categoria")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Categoria")
 
     def __str__(self):
         return self.name
@@ -51,8 +51,8 @@ class Service(models.Model):
     name = models.CharField(max_length=50, verbose_name="Nome", unique=True)
     description = models.TextField(blank=True, null=True, verbose_name="Descrição")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
-    delivery_time = models.IntegerField(verbose_name="Tempo de Entrega")
-    delivery_time_unit = models.CharField(max_length=10, choices=delivery_time_choices, default='hj', verbose_name="Unidade de Tempo", help_text="Tempo de entrega " \
+    delivery_time = models.IntegerField(verbose_name="Tempo de Entrega", blank=True, null=True)
+    delivery_time_unit = models.CharField(max_length=10, choices=delivery_time_choices, blank=True, default='hj', verbose_name="Unidade de Tempo", help_text="Tempo de entrega " \
     " em Minutos, Horas ou Dias")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Categoria")
 
